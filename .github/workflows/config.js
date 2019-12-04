cdb130c384d65bf8ec27c754de02bdf2cc9de361
Tetiana Gron <tgron@zendesk.com>
2019-12-04 13:02:58 +0100

    Move lighthouse settings to a new file

---
+++ .github/workflows/config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    extends: 'lighthouse:default',
+
+    settings: {
+        // For the purpose of this project, performance should be enough
+        // It can always be extended to other categories
+        onlyCategories: ['accessibility']
+    }
+};
